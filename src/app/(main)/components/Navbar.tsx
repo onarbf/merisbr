ca6ef31df228d65c8a4248539752e7d78ca57fd8
Onar Berrade <onarbf@gmail.com>
2023-09-12 19:37:23 +0200

    --

--- src/app/(main)/components/Navbar.tsx
+++ src/app/(main)/components/Navbar.tsx
@@ -2,7 +2,6 @@
 import Link from "next/link";
 import { useEffect } from "react";
 import { useGlobalContext } from "../context/store";
-import MarkerIcon from "../icons/marker-icon";
 
 export default function Navbar() {
     const { categories, setCategories } = useGlobalContext();
@@ -36,11 +35,11 @@ export default function Navbar() {
         py-4 px-8 bg-white border-b border-slate-600 mb-2">
         <span className="text-2xl cursor-pointer font-bold" onClick={() => toggleSetCategory(null)} >María Barriga</span>
         <ul className="flex gap-4 text-xl pt-2">
-            <li className={`cursor-pointer ${isCategoryActive('Illustration') ? 'underline' : ''}`} onClick={() => toggleSetCategory('Illustration')}>  
+            <li className={`cursor-pointer `} onClick={() => toggleSetCategory('Illustration')}>  
 
                 <h4 className="">Illustration</h4>
             </li>
-            <li className={`cursor-pointer ${isCategoryActive('Motion') ? 'underline' : ''}`} onClick={() => toggleSetCategory('Motion')}>
+            <li className={`cursor-pointer `} onClick={() => toggleSetCategory('Motion')}>
             <h4>Motion</h4></li>
             <li><Link href="/about"><h4>About</h4></Link></li>
             <li><Link href="/contact"> <h4>Contact</h4></Link></li>
